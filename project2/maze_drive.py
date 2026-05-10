6cb92ec8285df92106fcfb0d22834cb586059ca3
PolyGon-13 <hyundiego@hanyang.ac.kr>
2026-05-10 16:13:25 +0900

    [Edit] near_thresh 0.10 to 0.14

--- project2/maze_drive.py
+++ project2/maze_drive.py
@@ -390,7 +390,7 @@ def choose_best_cmd(scan, prev_w, cmd_v):
         if len(ry) > 0:
             info_right = float(-np.max(ry))
 
-    near_thresh = 0.1
+    near_thresh = 0.14
 
     best_w = 0.0
     best_score = -float("inf")
