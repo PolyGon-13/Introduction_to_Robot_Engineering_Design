c3e2dd06b8f401a4456f8cdd617d192013c1563b
leesihyeun <106770227+leesihyeun@users.noreply.github.com>
2026-05-18 03:21:44 +0900

    [edit]

--- project2/test/test.py
+++ project2/test/test.py
@@ -39,9 +39,9 @@ def compute_wheel_phis(target_angle_deg: float):
         else : target_angle_deg + 10
         
     if  -35 < target_angle_deg < 35 :
-        Kp_ang = 5.3
+        Kp_ang = 1
     
-    else : Kp_ang = 3.4
+    else : Kp_ang = 1
         
     angle_err_rad = math.radians(target_angle_deg)
     w = -Kp_ang * angle_err_rad
