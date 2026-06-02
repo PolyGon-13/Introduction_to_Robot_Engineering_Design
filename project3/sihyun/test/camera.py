c5ad0e0faa82f7666a3d55a063177af7f93b4065
leesihyeun <106770227+leesihyeun@users.noreply.github.com>
2026-06-02 18:45:24 +0900

    [eedit]노랑색 hsv수정

--- project3/sihyun/test/camera.py
+++ project3/sihyun/test/camera.py
@@ -22,7 +22,7 @@ CAMERA_ROTATION = cv2.ROTATE_90_COUNTERCLOCKWISE
 HSV_RANGES = {
     "RED": [([0, 160, 120], [5, 255, 255]), ([165, 160, 120], [179, 255, 255])],
     "BLUE": [([104, 90, 90], [116, 255, 230])],
-    "YELLOW": [([19, 130, 170], [25, 255, 255])],
+    "YELLOW": [([20, 100, 170], [28, 255, 255])],
 }
 HSV_RANGES = {
     name: [(np.array(lower, dtype=np.uint8), np.array(upper, dtype=np.uint8)) for lower, upper in ranges]
