81bbcf83de5684cf4693823364f65b1e12dabea3
leesihyeun <106770227+leesihyeun@users.noreply.github.com>
2026-06-19 21:52:19 +0900

    [edit]

--- project3/sihyun/test_wall/camera.py
+++ project3/sihyun/test_wall/camera.py
@@ -20,9 +20,9 @@ DEADBAND = 0.08
 CAMERA_ROTATION = cv2.ROTATE_90_COUNTERCLOCKWISE
 
 HSV_RANGES = {
-    "RED": [([0, 160, 120], [5, 255, 255]), ([165, 160, 120], [179, 255, 255])],
-    "BLUE": [([104, 90, 90], [116, 255, 255])],
-    "YELLOW": [([20, 55, 160], [35, 255, 255])],
+    "RED": [([0, 120, 100], [8, 255, 255]), ([160, 120, 100], [179, 255, 255])],
+    "BLUE": [([100, 80, 80], [120, 255, 255])],
+    "YELLOW": [([20, 90, 150], [35, 255, 255])],
 }
 HSV_RANGES = {
     name: [(np.array(lower, dtype=np.uint8), np.array(upper, dtype=np.uint8)) for lower, upper in ranges]
