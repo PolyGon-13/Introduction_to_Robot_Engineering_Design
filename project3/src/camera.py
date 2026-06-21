8007d79164923e655b73112637abc37173cbd737
leesihyeun <106770227+leesihyeun@users.noreply.github.com>
2026-06-22 05:56:00 +0900

    [edit]640 360

--- project3/src/camera.py
+++ project3/src/camera.py
@@ -39,7 +39,7 @@ def clamp(value, low, high):
 
 def open_camera():
     cam = Picamera2()
-    cam.configure(cam.create_preview_configuration(main={"format": "RGB888", "size": (640, 400)}))
+    cam.configure(cam.create_preview_configuration(main={"format": "RGB888", "size": (640, 360)}))
     cam.start()
     return cam
 
